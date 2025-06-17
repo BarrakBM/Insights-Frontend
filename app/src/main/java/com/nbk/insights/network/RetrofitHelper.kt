25550e89195066586f578de1d8657a5d8245284b
ahjadi <alihussainaljadi@gmail.com>
2025-06-17 09:48:58 +0300

    Added Constants.kt file and variable for port number -- Please make sure to edit this depending on your port number before you run

--- app/src/main/java/com/nbk/insights/network/RetrofitHelper.kt
+++ app/src/main/java/com/nbk/insights/network/RetrofitHelper.kt
@@ -1,12 +1,13 @@
 package com.nbk.insights.network
 
+import com.nbk.insights.utils.Constants
 import com.nbk.insights.utils.TokenManager
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    private const val baseUrl = "http://10.0.2.2:8080/"
+    private const val baseUrl = "http://10.0.2.2:${Constants.portNumber}/"
 
     fun getInstance(tokenManager: TokenManager): Retrofit {
         val okHttpClient = OkHttpClient.Builder()
---
+++ app/src/main/java/com/nbk/insights/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.nbk.insights.utils
+
+class Constants {
+    companion object{
+        // change the port number here
+        const val portNumber = "4444"
+    }
+}
