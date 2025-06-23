a57d7e836f197e8ca83fb06b2c80f15a808577e3
ahjadi <alihussainaljadi@gmail.com>
2025-06-23 15:47:20 +0300

    serialization for datetime. works and does not break existing work.

--- app/src/main/java/com/nbk/insights/network/RetrofitHelper.kt
+++ app/src/main/java/com/nbk/insights/network/RetrofitHelper.kt
@@ -1,10 +1,15 @@
 package com.nbk.insights.network
 
+import com.google.gson.*
 import com.nbk.insights.utils.Constants
 import com.nbk.insights.utils.TokenManager
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Type
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object RetrofitHelper {
     private const val baseUrl = "http://10.0.2.2:${Constants.portNumber}/"
@@ -16,10 +21,39 @@ object RetrofitHelper {
             })
             .build()
 
+        val gson = GsonBuilder()
+            .registerTypeAdapter(LocalDate::class.java, LocalDateAdapter())
+            .registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeAdapter())
+            .create()
+
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
+    }
+
+    class LocalDateAdapter : JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+        private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
+
+        override fun serialize(src: LocalDate?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+            return if (src == null) JsonNull.INSTANCE else JsonPrimitive(src.format(formatter))
+        }
+
+        override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): LocalDate? {
+            return if (json == null || json.isJsonNull) null else LocalDate.parse(json.asString, formatter)
+        }
+    }
+
+    class LocalDateTimeAdapter : JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+        private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+        override fun serialize(src: LocalDateTime?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+            return if (src == null) JsonNull.INSTANCE else JsonPrimitive(src.format(formatter))
+        }
+
+        override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): LocalDateTime? {
+            return if (json == null || json.isJsonNull) null else LocalDateTime.parse(json.asString, formatter)
+        }
     }
 }
