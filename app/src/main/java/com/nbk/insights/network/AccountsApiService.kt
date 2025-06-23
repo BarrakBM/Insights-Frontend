cd1fdabe39d220c905b32f846905bd916900e729
ahjadi <alihussainaljadi@gmail.com>
2025-06-23 08:26:06 +0300

    set limit api call

--- app/src/main/java/com/nbk/insights/network/AccountsApiService.kt
+++ app/src/main/java/com/nbk/insights/network/AccountsApiService.kt
@@ -3,8 +3,10 @@ package com.nbk.insights.network
 
 import com.nbk.insights.data.dtos.AccountsResponse
 import com.nbk.insights.data.dtos.BudgetAdherenceResponse
+import com.nbk.insights.data.dtos.LimitsRequest
 import com.nbk.insights.data.dtos.TotalBalanceResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -16,6 +18,9 @@ interface AccountsApiService {
 
     @GET("accounts/total-balance")
     suspend fun retrieveTotalBalance(): Response<TotalBalanceResponse>
+
+    @POST("accounts/limit")
+    suspend fun setAccountLimit(@Body request: LimitsRequest): Response<Map<String, String>>
 
     @POST("accounts/limits/deactivate/{limitId}")
     suspend fun deactivateLimit(@Path("limitId") limitId: Long): Response<Map<String, String>>
